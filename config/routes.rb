c55b6707ebbe5d0bb163502393f88e18100a59b3
tushancse04 <tushancse@gmail.com>
2017-04-02 00:53:12 +0000

    destination page partially created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :destinations
+  get 'destinations/index'
+
+  root 'destinations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Destination.create!(name: 'Walt Disney World', city: 'Orlando', state: 'Florida')
+Destination.create!(name: 'Las Vegas Strip', city: 'Las Vegas', state: 'Nevada')
+Destination.create!(name: 'Grand Central Terminal', city: 'New York', state: 'New York')
+Destination.create!(name: 'Magic Kingdom', city: 'Orlando', state: 'Florida')
+Destination.create!(name: 'Disneyland Resort', city: 'Anaheim', state: 'California')
+Destination.create!(name: 'Golden Gate Bridge', city: 'San Francisco', state: 'California')
+Destination.create!(name: 'Faneuil Hall Marketplace', city: 'Boston', state: 'Massachusetts')
+Destination.create!(name: 'Golden Gate Park', city: 'San Francisco', state: 'California')
+Destination.create!(name: 'Pike Place Market', city: 'Seattle', state: 'Washington')
+Destination.create!(name: 'South Street Seaport', city: 'New York', state: 'New York')
+
+

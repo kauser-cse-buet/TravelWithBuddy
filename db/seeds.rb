555c9511fae1c2a6ca5099c1525b3228907e4ba2
Md. Kauser Ahmmed <anik2137@gmail.com>
2017-04-02 22:36:57 -0500

    Added seed for user and admin.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,22 @@ TravelEvent.create(name: 'First Ever Event',
 end
 
 
-User.create!(first_name: 'Kauser', last_name: 'Ahmed', address: 'memphis', phone: '9013154477', email: 'ahmed@memphis.edu')
-User.create!(first_name: 'Isaac', last_name: 'Roland', address: 'memphis', phone: '9013158877', email: 'isaac@memphis.edu')
-User.create!(first_name: 'Maminur', last_name: 'Islam', address: 'memphis', phone: '9013154467', email: 'mislam@memphis.edu')
-User.create!(first_name: 'Nazmul', last_name: 'Hasan', address: 'memphis', phone: '9013154477', email: 'ahmed@memphis.edu')
+admin_ahmmed = Admin.create!(email: 'ahmmed@memphis.edu', password: '123456', password_confirmation: '123456')
+admin_isaac = Admin.create!(email: 'isaac@memphis.edu', password: '123456', password_confirmation: '123456')
+admin_mislam = Admin.create!(email: 'mislam@memphis.edu', password: '123456', password_confirmation: '123456')
+admin_milu = Admin.create!(email: 'milu@memphis.edu', password: '123456', password_confirmation: '123456')
 
+ahmmed = User.create!(first_name: 'Kauser', last_name: 'Ahmed', address: 'memphis', phone: '9013154477', email: admin_ahmmed.email)
+isaac = User.create!(first_name: 'Isaac', last_name: 'Roland', address: 'memphis', phone: '9013158877', email: admin_isaac.email)
+mislam = User.create!(first_name: 'Maminur', last_name: 'Islam', address: 'memphis', phone: '9013154467', email: admin_mislam.email)
+milu = User.create!(first_name: 'Milu', last_name: 'Hasan', address: 'memphis', phone: '9013154477', email: admin_milu.email)
+
+admin_ahmmed.user = ahmmed
+admin_isaac.user = isaac
+admin_mislam.user = mislam
+admin_milu.user = milu
+
+admin_ahmmed.save
+admin_isaac.save
+admin_mislam.save
+admin_milu.save

c3f784a94efff1e344f80e32637f8eb06f239939
Md. Kauser Ahmmed <anik2137@gmail.com>
2017-04-06 14:45:24 -0500

    Fix image uploader proble.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -37,6 +37,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
+
+  version :passport do
+    process resize_to_fit: [100, 100]
+  end
+
+  version :normal do
+    process resize_to_fit: [300, 300]
+  end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:

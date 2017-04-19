f1e5ed1ed75f92f21d1aaaaf8cad1dd6ab6f5026
Md. Kauser Ahmmed <anik2137@gmail.com>
2017-04-19 07:19:24 -0500

    Add ajax for write post to travel event.

--- app/controllers/travel_events_controller.rb
+++ app/controllers/travel_events_controller.rb
@@ -1,153 +1,156 @@
 class TravelEventsController < ApplicationController
-  before_action :set_travel_event, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin!
+	before_action :set_travel_event, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_admin!
 
-  # GET /travel_events
-  # GET /travel_events.json
-  def index
-	
-	@filterrific = initialize_filterrific(
-      TravelEvent.where('travel_events.start > ?',DateTime.now.to_s(:db)),
-      params[:filterrific],
-    ) or return
-	
-	@travel_events = @filterrific.find.page(params[:page])
-	
-	respond_to do |format|
-      format.html
-      format.js
-    end
-  end
-  
-  def past_events
-	@filterrific = initialize_filterrific(
-      TravelEvent.where('travel_events.start <= ?',DateTime.now.to_s(:db)),
-      params[:filterrific],
-    ) or return
-	
-	@travel_events = @filterrific.find.page(params[:page])
-	
-	respond_to do |format|
-      format.html
-      format.js
-    end
-  end
+	# GET /travel_events
+	# GET /travel_events.json
+	def index
 
-  # GET /travel_events/1
-  # GET /travel_events/1.json
-  def show
-    @travel_event = TravelEvent.find(params[:id])
-    @reviews = Review.where(travel_event_id: @travel_event.id)
-    @posts = Post.where(travel_event_id: @travel_event.id)
+		@filterrific = initialize_filterrific(
+			TravelEvent.where('travel_events.start > ?', DateTime.now.to_s(:db)),
+			params[:filterrific],
+		) or return
 
-    @reviews_paginated = @reviews.paginate(:page => params[:reviews_page], :per_page => 4).order('created_at DESC')
-    @posts_paginated = @posts.paginate(:page => params[:posts_page], :per_page => 4).order('created_at DESC')
+		@travel_events = @filterrific.find.page(params[:page])
 
-    @users = User.all
-    @has_review = @reviews.where(user_id:current_admin.user.id).count > 0
-    @average_rating = @reviews.average(:rating)
-  end
+		respond_to do |format|
+			format.html
+			format.js
+		end
+	end
 
-  # GET /travel_events/new
-  def new
-    @travel_event = TravelEvent.new
-  end
+	def past_events
+		@filterrific = initialize_filterrific(
+			TravelEvent.where('travel_events.start <= ?', DateTime.now.to_s(:db)),
+			params[:filterrific],
+		) or return
 
-  # GET /travel_events/1/edit
-  def edit
-  end
+		@travel_events = @filterrific.find.page(params[:page])
 
-  def invitations
-    @invitations = Invitation.all.where(user_id: current_admin.user.id)
-  end
+		respond_to do |format|
+			format.html
+			format.js
+		end
+	end
 
-  def join
-    
-  end
+	# GET /travel_events/1
+	# GET /travel_events/1.json
+	def show
+		@travel_event = TravelEvent.find(params[:id])
+		@reviews = Review.where(travel_event_id: @travel_event.id).paginate(:page => params[:reviews_page], :per_page => 4).order('created_at DESC')
+		@posts = Post.where(travel_event_id: @travel_event.id).paginate(:page => params[:posts_page], :per_page => 4).order('created_at DESC')
 
-  def join_event
-    @travel_event_id = params[:id]
-    Attendee.create(paid: true, user_id:current_admin.user.id,travel_event_id:@travel_event_id)
-  end
+		@reviews_paginated = @reviews.paginate(:page => params[:reviews_page], :per_page => 4).order('created_at DESC')
+		@posts_paginated = @posts.paginate(:page => params[:posts_page], :per_page => 4).order('created_at DESC')
 
-  def create_invitations
-    @user_ids = params[:user_ids]
-    @des = params[:description]
-    @from = User.all.where(admin_id:current_admin.id).first().id
-    error = false
-    @user_ids.each do |id|
-      @invitation = Invitation.create(description: @des,from: @from, user_id: id,travel_event_id: params[:id])
-      if !@invitation.id
-        error = true
-      end
-    end
-    if !error
-      redirect_to home_url
-    else
+		@users = User.all
+		@has_review = @reviews.where(user_id: current_admin.user.id).count > 0
+		@average_rating = @reviews.average(:rating)
 
-    end
-  end
+		@post = Post.new(travel_event_id: @travel_event.id,
+						 user_id: current_admin.id)
+	end
 
-  def new_invitations
-    @event = TravelEvent.find(params[:id])
+	# GET /travel_events/new
+	def new
+		@travel_event = TravelEvent.new
+	end
 
-    @invitations = []
-    User.all.each do |user|
-      @invitation = Invitation.new
-      @invitation.travel_event_id = params[:id]
-      @invitation.user_id = user.id
-      @invitations << @invitation
-    end
-  end
+	# GET /travel_events/1/edit
+	def edit
+	end
 
-  # POST /travel_events
-  # POST /travel_events.json
-  def create
-    @travel_event = TravelEvent.new(travel_event_params)
+	def invitations
+		@invitations = Invitation.all.where(user_id: current_admin.user.id)
+	end
 
-    respond_to do |format|
-      if @travel_event.save
-        format.html { redirect_to @travel_event, notice: 'Travel event was successfully created.' }
-        format.json { render :show, status: :created, location: @travel_event }
-      else
-        format.html { render :new }
-        format.json { render json: @travel_event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	def join
 
-  # PATCH/PUT /travel_events/1
-  # PATCH/PUT /travel_events/1.json
-  def update
-    respond_to do |format|
-      if @travel_event.update(travel_event_params)
-        format.html { redirect_to @travel_event, notice: 'Travel event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @travel_event }
-      else
-        format.html { render :edit }
-        format.json { render json: @travel_event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	end
 
-  # DELETE /travel_events/1
-  # DELETE /travel_events/1.json
-  def destroy
-    @travel_event.destroy
-    respond_to do |format|
-      format.html { redirect_to travel_events_url, notice: 'Travel event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+	def join_event
+		@travel_event_id = params[:id]
+		Attendee.create(paid: true, user_id: current_admin.user.id, travel_event_id: @travel_event_id)
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_travel_event
-      @travel_event = TravelEvent.find(params[:id])
-    end
+	def create_invitations
+		@user_ids = params[:user_ids]
+		@des = params[:description]
+		@from = User.all.where(admin_id: current_admin.id).first().id
+		error = false
+		@user_ids.each do |id|
+			@invitation = Invitation.create(description: @des, from: @from, user_id: id, travel_event_id: params[:id])
+			if !@invitation.id
+				error = true
+			end
+		end
+		if !error
+			redirect_to home_url
+		else
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def travel_event_params
-      params.require(:travel_event).permit(:name, :description, :max_attendance, :price, :start, :duration,:travel_destination_id)
-    end
+		end
+	end
+
+	def new_invitations
+		@event = TravelEvent.find(params[:id])
+
+		@invitations = []
+		User.all.each do |user|
+			@invitation = Invitation.new
+			@invitation.travel_event_id = params[:id]
+			@invitation.user_id = user.id
+			@invitations << @invitation
+		end
+	end
+
+	# POST /travel_events
+	# POST /travel_events.json
+	def create
+		@travel_event = TravelEvent.new(travel_event_params)
+
+		respond_to do |format|
+			if @travel_event.save
+				format.html {redirect_to @travel_event, notice: 'Travel event was successfully created.'}
+				format.json {render :show, status: :created, location: @travel_event}
+			else
+				format.html {render :new}
+				format.json {render json: @travel_event.errors, status: :unprocessable_entity}
+			end
+		end
+	end
+
+	# PATCH/PUT /travel_events/1
+	# PATCH/PUT /travel_events/1.json
+	def update
+		respond_to do |format|
+			if @travel_event.update(travel_event_params)
+				format.html {redirect_to @travel_event, notice: 'Travel event was successfully updated.'}
+				format.json {render :show, status: :ok, location: @travel_event}
+			else
+				format.html {render :edit}
+				format.json {render json: @travel_event.errors, status: :unprocessable_entity}
+			end
+		end
+	end
+
+	# DELETE /travel_events/1
+	# DELETE /travel_events/1.json
+	def destroy
+		@travel_event.destroy
+		respond_to do |format|
+			format.html {redirect_to travel_events_url, notice: 'Travel event was successfully destroyed.'}
+			format.json {head :no_content}
+		end
+	end
+
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_travel_event
+		@travel_event = TravelEvent.find(params[:id])
+	end
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def travel_event_params
+		params.require(:travel_event).permit(:name, :description, :max_attendance, :price, :start, :duration, :travel_destination_id)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :travel_events
   root to: "travel_events#index"
- 
+
   end

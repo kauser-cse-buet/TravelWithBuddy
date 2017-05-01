958262d4aa4ec1753cca1ddd2942b7786dcd7b2f
Md. Kauser Ahmmed <anik2137@gmail.com>
2017-05-01 16:56:12 -0500

    destroy invitation on successful join.

--- app/controllers/travel_events_controller.rb
+++ app/controllers/travel_events_controller.rb
@@ -81,13 +81,18 @@ class TravelEventsController < ApplicationController
 		@travel_event_id = params[:id]
 		@travel_event = TravelEvent.find(@travel_event_id)
 		@user = User.all.where(admin_id: current_admin.id).first()
+
+		@invitation = Invitation.where(user_id: current_admin.user.id, travel_event_id: @travel_event_id).take
+
 		if @travel_event.attendees.where(user_id: @user.id).count > 0
 			redirect_to travel_event_url(@travel_event), notice: 'You already had joined the event!' and return
 		end
 		if @travel_event.attendees.count < @travel_event.max_attendance
 			@attendee = Attendee.create(paid: true, user_id: @user.id, travel_event_id: @travel_event_id)
 			if @attendee.id
+				@invitation.destroy
 				redirect_to travel_event_url(@travel_event), notice: 'You joined the event successfully.'
+
 			else
 				redirect_to travel_event_url(@travel_event), notice: 'Error in joining event'
 			end
